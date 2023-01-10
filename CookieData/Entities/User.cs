abfce9eb82738ae7f82ec2e73a1030a21e36f114
Relixxxy <vut1111111@gmail.com>
2023-01-10 15:02:04 +0200

    user added

---
+++ CookieData/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CookieData.Entities
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }
+        [JsonIgnore]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
