9187be0fc6639392f1beba6c009a7ecc9a52d156
Relixxxy <vut1111111@gmail.com>
2023-01-11 15:08:57 +0200

    name deleted

---
+++ CookieData/Model/UpgradeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CookieData.Model
+{
+    public class UpgradeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Price { get; set; }
+        public int BaseTick { get; set; }
+        public int Level { get; set; }
+        public int Amount { get; set; }
+        public int GameAccountId { get; set; }
+    }
+}
