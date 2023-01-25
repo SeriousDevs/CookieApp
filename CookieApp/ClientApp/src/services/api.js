978d0be27cc3c60ea86910e84ea19a9a2aba54e4
Relixxxy <vut1111111@gmail.com>
2023-01-25 19:13:47 +0200

    url changed to azure

--- CookieApp/ClientApp/src/services/api.js
+++ CookieApp/ClientApp/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const $publicHost = axios.create({
-  baseURL: "https://localhost:7184/api",
+  baseURL: "https://cookieapp.azurewebsites.net/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const $privateHost = axios.create({
-  baseURL: "https://localhost:7184/api",
+  baseURL: "https://cookieapp.azurewebsites.net/api",
   headers: {
     "Content-Type": "application/json",
   },
